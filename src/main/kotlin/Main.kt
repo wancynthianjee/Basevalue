03c80cee8bb057972a5e12450ea835fb59a463fe
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-05-03 09:08:46 +0300

    commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+//calculate the power of a number
+
+fun main() {
+    val base = 3
+    var exponent = 4
+    var result = 1
+    while (exponent != 0){
+        result = result * base
+    -- exponent
+}
+
+    println("Answer = $result")
+}
+
